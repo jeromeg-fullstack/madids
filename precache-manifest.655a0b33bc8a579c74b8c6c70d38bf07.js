aa0f5bd66164456ce52ecbdbbe9e9145aa505690
jeromeg-fullstack <68829003+jeromeg-fullstack@users.noreply.github.com>
2020-12-23 18:43:59 +0800

    Add files via upload

---
+++ precache-manifest.655a0b33bc8a579c74b8c6c70d38bf07.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592667fcbfa4744b1cfb2bbfa31b227e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53231c499a37695b6d0",
+    "url": "/static/css/2.3a13669e.chunk.css"
+  },
+  {
+    "revision": "c83c51d53c4912183a44",
+    "url": "/static/css/main.10d678ab.chunk.css"
+  },
+  {
+    "revision": "e53231c499a37695b6d0",
+    "url": "/static/js/2.1827b4b6.chunk.js"
+  },
+  {
+    "revision": "39473544d74cced5be6d8895cef840b5",
+    "url": "/static/js/2.1827b4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83c51d53c4912183a44",
+    "url": "/static/js/main.7130a892.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "566dccbcfb2c9e81ae72a2dcce81cfb4",
+    "url": "/static/media/1.566dccbc.jpg"
+  },
+  {
+    "revision": "c38360ea5a54c949f976fa06b8b997ce",
+    "url": "/static/media/bank-note.c38360ea.svg"
+  },
+  {
+    "revision": "604a5a0525bacbee31356f10beeabe46",
+    "url": "/static/media/bed.604a5a05.svg"
+  },
+  {
+    "revision": "e2b1e9f656d1cfe99c0d82ce61f0f491",
+    "url": "/static/media/birthday-cake.e2b1e9f6.svg"
+  },
+  {
+    "revision": "9bb27cccac4767a581b5269284f04a1c",
+    "url": "/static/media/blind.9bb27ccc.svg"
+  },
+  {
+    "revision": "47491429ded7753ea93398b63ea7d0bd",
+    "url": "/static/media/cave1.47491429.jpg"
+  },
+  {
+    "revision": "c3edf6b5da0be6dcbb2613a01a2f315f",
+    "url": "/static/media/cave1.c3edf6b5.jpg"
+  },
+  {
+    "revision": "1585abe08f038269d2e30515a78e027c",
+    "url": "/static/media/credit-card.1585abe0.svg"
+  },
+  {
+    "revision": "71d094a8d7d0ccd21ca2c9ac3fb81f94",
+    "url": "/static/media/cutlery.71d094a8.svg"
+  },
+  {
+    "revision": "73caaa01e4d700f4b23e47665ecc2727",
+    "url": "/static/media/facebook.73caaa01.svg"
+  },
+  {
+    "revision": "9ce6986b0dfef2c89e673c3d65635629",
+    "url": "/static/media/fire-extinguisher.9ce6986b.svg"
+  },
+  {
+    "revision": "0b117993cfdc06f6a05973ffa37ab7dc",
+    "url": "/static/media/instagram.0b117993.svg"
+  },
+  {
+    "revision": "633890dbaa6dbfbe744ad0edff5e1b51",
+    "url": "/static/media/logo.633890db.svg"
+  },
+  {
+    "revision": "6127ad1f8b29b0c2f1b7e5c7ceaa2c4c",
+    "url": "/static/media/resort1.6127ad1f.jpg"
+  },
+  {
+    "revision": "644e4952bfc42a7297e764fa02df2d6d",
+    "url": "/static/media/resort3.644e4952.jpg"
+  },
+  {
+    "revision": "dd5bef1b062600998c879773506132fa",
+    "url": "/static/media/shopping-cart.dd5bef1b.svg"
+  },
+  {
+    "revision": "ec29d2d2d06eab6d926b0cbf35fe6639",
+    "url": "/static/media/shower.ec29d2d2.svg"
+  },
+  {
+    "revision": "435fc4bc44c1e90d5f00f4efccdb9b1d",
+    "url": "/static/media/twitter.435fc4bc.svg"
+  },
+  {
+    "revision": "1670da7c5b6cc551d0ee502b9c26b3ad",
+    "url": "/static/media/wifi.1670da7c.svg"
+  }
+]);
